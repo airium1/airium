b689467d83346b0a54830b1ace1d1b2e6b77ff02
airium1 <150402754+airium1@users.noreply.github.com>
2023-11-16 13:40:28 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// main_slide banner-main
+
+$(function(){
+    setInterval(function(){
+        $("#main_slide ul").delay(2500)
+        $("#main_slide ul").animate({marginLeft:'-1920px'})
+        $("#main_slide ul").delay(2500)
+        $("#main_slide ul").animate({marginLeft:'-3840px'})
+        $("#main_slide ul").delay(2500)
+        $("#main_slide ul").animate({marginLeft:'0'})
+    });
+});
+
+// section_2_method_main 
+
+$(function(){
+    setInterval(function(){
+        $("#method_slide ul").delay(2500)
+        $("#method_slide ul").animate({marginLeft:'-1920px'})
+        $("#method_slide ul").delay(2500)
+        $("#method_slide ul").animate({marginLeft:'0'})
+    });
+});
+
